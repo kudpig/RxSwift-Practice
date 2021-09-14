6a18d674bd7984f79ca97b0f3b3fb71ad08249e2
kudpig <62790209+kudpig@users.noreply.github.com>
2021-09-14 16:08:03 +0900

    bind

--- RxSwift-Practice/Samples/Third/ThirdSampleViewController.swift
+++ RxSwift-Practice/Samples/Third/ThirdSampleViewController.swift
@@ -6,15 +6,31 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ThirdSampleViewController: UIViewController {
-
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        textField.rx.text
+            .map { text -> String? in
+                guard let text = text else { return nil }
+                return "いま\(text.count)文字"
+            }
+            .bind(to: countLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        textField.rx.text
+            .bind(to: nameLabel.rx.text)
+            .disposed(by: disposeBag)
     }
-    
-
-   
 
 }
